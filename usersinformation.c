b871062cafd418e52c857b21622b2322e7610eb7
Fatima Javed <k250594@nu.edu.pk>
2025-09-09 20:16:03 +0500

    Update usersinformation.c

--- usersinformation.c
+++ usersinformation.c
@@ -1,1 +1,20 @@
+// Q:09 Write a C program that asks the user's name and age and print them use escape sequences as well. 
 
+#include <stdio.h>
+
+int main(void)
+{
+    char name[20];
+    int age;
+    
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("\n Hello, %s\n", name);
+    printf("Your age is %d\n", age);
+
+    return 0;
+}
