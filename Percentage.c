e239f3e8c9b84fa49be3bd4a2cad6b801c6e62ca
Fatima Javed <k250594@nu.edu.pk>
2025-09-09 19:54:34 +0500

    Update Percentage.c

--- Percentage.c
+++ Percentage.c
@@ -2,3 +2,28 @@
 Q:08
 ASK THE USER FOR THEIR MARKS IN THREE SUBJECTS AND CALCULATE THE PERCENTAGE.
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    float subj1, subj2, subj3;
+    float obtainedmarks;
+    float percentage;
+    
+    printf("Enter the marks of subj1: ");
+    scanf("%f", &subj1);
+
+    printf("Enter the marks of subj2: ");
+    scanf("%f", &subj2);
+
+    printf("Enter the marks of subj3: ");
+    scanf("%f", &subj3);
+
+    obtainedmarks = subj1+subj2+subj3;
+    percentage = (obtainedmarks/300)*100;
+    printf("The percentage is = %f", percentage);
+
+    return 0;
+
+}
