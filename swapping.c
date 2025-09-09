d5271f59c3d7f08d8c78a94e1f9f359f2ac1d405
Fatima Javed <k250594@nu.edu.pk>
2025-09-09 20:42:59 +0500

    Update swapping.c

--- swapping.c
+++ swapping.c
@@ -3,3 +3,26 @@ Q:10
 Write a program to swap two variables using a third variable.
 Repeat the program again without using a third variable.
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int f;
+    int a;
+    int b;
+
+    printf("Enter number f: ");
+    scanf("%d", &f);
+
+    printf("Enter number a: ");
+    scanf("%d", &a);
+    b = f;
+    f = a;
+    a = b;
+
+    printf("f is %d\n", f);
+    printf("a is %d\n", a);
+
+    return 0;
+}
